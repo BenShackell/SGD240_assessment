9c07953b2ae16b775c9bfe6ee8f33d37eaee4306
BenShackell <bjs065@student.usc.edu.au>
2024-10-27 13:04:16 +1000

    Infinite mesh generator
    Ran into a couple of bugs, resorted to running it back to older version to figure it it, still have a bug where the mesh shader isn't applying

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,18 +4,17 @@ using System.Collections;
 public static class MeshGenerator
 {
 
-    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve _heightCurve, int levelOfDetail)
     {
+        AnimationCurve heightCurve = new AnimationCurve(_heightCurve.keys);
+
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
-
         float topLeftX = (width - 1) / -2f;
         float topLeftZ = (height - 1) / 2f;
 
         int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
         int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
-
-        Debug.Log($"Width: {width}, height: {height}, increment: {meshSimplificationIncrement}");
 
         MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
         int vertexIndex = 0;
